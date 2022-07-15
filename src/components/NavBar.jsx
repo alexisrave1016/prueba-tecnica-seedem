943b1538f8f1fefdb27f44cc0f9cb33450b90795
alexisRave <alexis9876@hotmail.com>
2022-07-15 16:31:05 -0500

    prueba

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../css/navBar.css'
+
+export default function NavBar() {
+  return (
+    <div className='container-navBar'>
+
+        <div className="container-opciones">
+            <div className="logo">
+                <img src="https://res.cloudinary.com/dd01p9lb9/image/upload/v1657919555/seedem/logo_naj2id.svg" alt="" srcset="" />
+            </div>
+            <div className="opciones">
+                <h2 className='seleccion-opciones'>Feactures</h2>
+                <h2 className='seleccion-opciones'>Princing</h2>
+                <h2 className='seleccion-opciones'>Resources</h2>
+            </div>
+        </div>
+
+        <div className="login-registro">
+            <div className='login'>Login</div>
+            <div className='registro'>Sign Up</div>
+        </div>
+
+    </div>
+  )
+}
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 export default function App() {
   return (
-    <div>Hola mundo</div>
+    <div>
+        <NavBar/>
+    </div>
   )
 }
