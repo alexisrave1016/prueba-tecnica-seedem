4a021bf9924a7a08e1137050e04703142e6eece6
alexisRave <alexis9876@hotmail.com>
2022-07-20 14:35:59 -0500

    act

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,25 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../css/buscador.css'
 
-
-const Buscador = ({setInputValue}) => {
-  // console.log(setInputValue + 'soy clic letra');
-
-  // const [tituloAdicional, setTituloAdicional] = useState(false);
+export default function Buscador({setInputValue}) {
   const [value, setValue] = useState('');
-
-  // const tituloApoyo=(e)=>{
-  //   e.preventDefault()
-  //     setTituloAdicional(!tituloAdicional)
-  // }
-
   const handlelick=()=>{
     setInputValue(value);
     setValue('');
-    // if(setValue.length=0){
-    //   return <p className='titulo-apoyo'>Please add a link</p>
-    // }
-    console.log('soy soy'+setValue(value));
   }
 
   
@@ -33,47 +19,8 @@ const Buscador = ({setInputValue}) => {
             value={value}
             onChange={e=>setValue(e.target.value)}
             />
-            {/* <span className={`titulo-apoyo ${value ?'ActivarTexto':''}`}>Please add a link</span> */}
             <button className='empezar' onClick={handlelick}>Shorten It!</button>
         </div>
     </div>
   )
 }
-
-export default Buscador
-
-// export default function Buscador({setInputValue}) {
-
-//   console.log(setInputValue);
-//   const [tituloAdicional, setTituloAdicional] = useState(false);
-// const [value, setValue] = useState('');
-
-//   // const tituloApoyo=(e)=>{
-//   //   e.preventDefault()
-//   //     setTituloAdicional(!tituloAdicional)
-//   // }
-
-//   const handlelick=(e)=>{
-//     e.preventDefault();
-//     setInputValue(value);
-//     setValue('');
-  
-//   }
-
-  
-//   return (
-//     <div className='contenedor-global-buscador'>
-//         <form className='contenedor-buscador' >
-//             <input 
-//             type="text" 
-//             placeholder='Shorten a link here...' 
-//             className='buscador-links' 
-//             value={value}
-//             onChange={e=>setValue(e.target.value)}
-//             />
-//             <span className={`titulo-apoyo ${tituloAdicional ?'ActivarTexto':''}`}>Please add a link</span>
-//             <button className='empezar' onClick={handlelick}>Shorten It!</button>
-//         </form>
-//     </div>
-//   )
-// }
--- src/components/LinkCorto.jsx
+++ src/components/LinkCorto.jsx
@@ -1,21 +1,18 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
-import '../css/app.css'
-import '../css/linkcorto.css'
+import React, { useState, useEffect } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import '../css/linkcorto.css';
 
-
-
-const LinkCorto = ({inputValue}) => {
+export default function LinkCorto({inputValue}) {
   const [linkRecontado, setLinkRecontado] = useState('')//soy el link ingresado para recortar
   const [copied, setCopied] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false)
 
-  const fetchData = async()=>{
+  const peticiones = async()=>{
     try{
       setLoading(true);
-      const res=await axios(`https://api.shrtco.de/v2/shorten?url=${inputValue}`);
+      const res= await axios(`https://api.shrtco.de/v2/shorten?url=${inputValue}`);
       setLinkRecontado(res.data.result.full_short_link);
     }catch(err){
         setError(err);
@@ -25,9 +22,10 @@ const LinkCorto = ({inputValue}) => {
 
   } 
 
+ 
   useEffect(() => {
     if(inputValue.length){
-      fetchData(); 
+      peticiones(); 
     }
   }, [inputValue])
 
@@ -47,72 +45,22 @@ const LinkCorto = ({inputValue}) => {
     return <p className='espera'>Something wne t wrong</p>
   }
 
-return (
-  <>
-  {linkRecontado && (
+  return (
+    <>
+    {linkRecontado && (
       <div className='contenedor-link'>
-      <p className='link-cortar'>{inputValue}</p>
-      <hr className='linea-link-corto'/>
-      <span className='link-resultado'>{linkRecontado}</span>
-      <CopyToClipboard 
-      text={linkRecontado}
-      onCopy={()=>setCopied(true)}>
-      <button className={`boton-copiar ${copied? 'copied':''}`}>Copy!</button>
-      </CopyToClipboard>
-  </div>
-  )}
+        <p className='link-cortar'>{inputValue}</p>
+        <hr className='linea-link-corto'/>
+        <span className='link-resultado'>{linkRecontado}</span>
+        <CopyToClipboard 
+          text={linkRecontado}
+          onCopy={()=>setCopied(true)}>
+          <button className={`boton-copiar ${copied? 'copied':''}`}>Copy!</button>
+        </CopyToClipboard>
+      </div>
+    )
+  }
   
   </>
 )
-}
-
-export default LinkCorto
-
-// export default function LinkCorto({inputValue}) {
-
-//     const [linkRecontado, setLinkRecontado] = useState('soy la prueba hasta elmomento')
-//     const [copied, setCopied] = useState(false);
-//     const [loading, setLoading] = useState(false);
-
-//     const fetchData=async()=>{
-//       try{
-//         setLoading(true);
-//         const res= axios(`https://api.shrtco.de/v2/shorten?url${inputValue}`);
-//         setLinkRecontado(res.data)
-//       }catch(err){
-
-//       }finally{
-
-//       }
-
-//     } 
-
-//     useEffect(() => {
-//       if(inputValue.length){
-//         fetchData(); 
-//       }
-//     }, [inputValue])
-
-
-//     useEffect(() => {
-//       const timer= setTimeout(() => {
-//         setCopied(false)
-//       }, 1000);
-
-//       return ()=>clearTimeout(timer);
-//     }, [copied])
-
-
-//     console.log(setLinkRecontado)
-//   return (
-//     <div className='contenedor-link'>
-//         <p className='link-cortar'>prueba de link</p>
-//         <span className='link-resultado'> prueba de resultado</span>
-//         <CopyToClipboard 
-//         text={linkRecontado}
-//         onCopy={()=>setCopied(true)}>
-//         <button className={`boton-copiar ${copied? 'copied':''}`}>Copied!</button>
-//         </CopyToClipboard>
-//     </div>
-//   )
-// }
+};
